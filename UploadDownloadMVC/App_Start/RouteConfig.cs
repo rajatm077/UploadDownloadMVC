4a28038eb8cc724623aca570097c97b79c0defc9
MalhotR1 <rajatmalhotra26@gmail.com>
2018-01-11 13:11:05 +0530

    File Upload action performed successfully.

--- UploadDownloadMVC/App_Start/RouteConfig.cs
+++ UploadDownloadMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UploadDownloadMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Upload", id = UrlParameter.Optional }
             );
         }
     }
---
+++ UploadDownloadMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadDownloadMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Upload() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file) {
+            try {
+                if (file.ContentLength > 0) {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+                    file.SaveAs(path);
+                    ViewBag.Message = "File uploaded successfully";
+                }
+
+            } catch (Exception e) {
+                    ViewBag.Message = "Upload failed!"; 
+                    //return RedirectToAction("Upload");
+            }
+
+            return View();
+        }
+    }
+}
